7450e306466bf28be956a8a3c21f68a73bdb1789
Rafael <Rafael@DESKTOP-B9MVOUG>
2025-06-13 10:52:00 -0600

    Semana 5 Modificaciones

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -9,24 +9,41 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoriaService {
-    
+
     //Crear un objeto de manera unica para todo el proyecto y de manera automatica
     @Autowired
     private CategoriaRepository categoriaRepository;
-    
-    @Transactional(readOnly=true)
-    public List<Categoria> getCategorias(boolean activo){
+
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activo) {
         var lista = categoriaRepository.findAll();
-        
-        
+
         //Se valida si solo se desean categorias activas:
-        if(activo){
+        if (activo) {
             //Solo se quieren activas:
             lista.removeIf(c -> !c.isActivo());
         }
-        
-        
+
         return lista;
-        
+
+    }
+
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaRepository.findById(categoria.getIdCategoria())
+                .orElse(null);
+
+    }
+
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaRepository.save(categoria);
+
+    }
+    
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaRepository.delete(categoria);
+
     }
 }
